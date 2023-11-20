816aae556af7ba2bf8deefc9e9928d220ff60224
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-20 09:36:51 -0700

    updated package.json

--- Main/src/App.jsx
+++ Main/src/App.jsx
@@ -1,15 +1,2 @@
-// We import useState and useEffect in our component
-import { useState, useEffect } from 'react';
-
-// import components
-
-function App() {
-  // We declare a state variable that is an array called `issues` and a function to update it.
-  const [repos, setRepos] = useState([]);
-
-  // When the page loads, set the document title, array is empty so it only runs once
-  useEffect(() => {
-    document.title = "Colton Laidig's Portfolio";
-  }, []);
-
-}
+// root imports
+import './App.css';
