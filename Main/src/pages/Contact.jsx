90580bfc9b08ace93cff3a16778ab5d2bea3fcb8
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-26 21:02:19 -0700

    added logic implementing a validating email helper and check to make sure the fowm is completed upon submission

--- Main/src/pages/Contact.jsx
+++ Main/src/pages/Contact.jsx
@@ -2,29 +2,55 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
 
+// helper 
+import { validateEmail } from "../utils/helpers";
+
 function Contact() {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [message, setMessage] = useState("");
+  const [FormSubmitted, setFormSubmitted] = useState(false);
 
   const handleInputChange = (event) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = event;
-    const inputType = target.name;
-    const inputValue = target.value;
-    console.log(event.target.value);
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'name') {
-      setName(inputValue);
-    } else {
-      setMessage(inputValue);
+    // Getting the value and name of the input which triggered the change when the form is not submitted
+    if (!FormSubmitted) {
+      const { target } = event;
+      const inputType = target.name;
+      const inputValue = target.value;
+      console.log(event.target.value);
+      // Based on the input type, we set the state of either email, username, and password
+      if (inputType === 'email') {
+        setEmail(inputValue);
+      } else if (inputType === 'name') {
+        setName(inputValue);
+      } else {
+        setMessage(inputValue);
+      }
     }
   };
 
+  // form submit handler
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // check if email is valid
+    if (!validateEmail(email)) {
+      alert('Please enter a valid email!');
+      return;
+    }
+    // check if form is filled out
+    if (name && email && message) {
+      // clear form
+      setName('');
+      setEmail('');
+      setMessage('');
+      setFormSubmitted(true);
+    } else {
+      alert('Please fill out all fields before submitting!');
+    }
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleFormSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control name='email' onChange={handleInputChange} value={email} type="email" placeholder="Enter email" />
