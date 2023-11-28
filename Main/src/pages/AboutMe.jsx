bd5dcda6436f125f6d4546705d61d84763a57cf0
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-28 14:44:49 -0700

    adding more styling and resume from docs

--- Main/src/pages/AboutMe.jsx
+++ Main/src/pages/AboutMe.jsx
@@ -1,13 +1,14 @@
 // imports 
 import "../style/AboutMe.css";
+import headshotImage from "../assets/placeholder.png";
 
 // function
 function AboutMe() {
     return (
         <div>
-            <h1>About Me</h1>
+            <img src={headshotImage} alt="headshot" className="headshot"/>
             <div className="content">
-                <p> My name is Colton and I am a Colorado based software engineer</p>
+                <p> My name is Colton and I am a Colorado based software engineer, you can check out more of my accounts via the links below, or reach out directly through the 'contact me' page.</p>
             </div>
         </div>
     )
--- Main/src/pages/Resume.jsx
+++ Main/src/pages/Resume.jsx
@@ -1,10 +1,18 @@
 // imports 
-
+import softwareResume from "../assets/software-resume.pdf";
 // function
 function Resume() {
     return (
         <div>
-            <h1>Resume</h1>
+            <h1>My Resume</h1>
+            <div>
+                <embed 
+                    src={softwareResume} 
+                    type="application/pdf"
+                    width="80%" 
+                    height="600px"
+                    />
+            </div>
         </div>
     )
 }
