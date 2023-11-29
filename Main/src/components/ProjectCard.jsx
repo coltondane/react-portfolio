72903460fb506dd131d41643e91811c579fd6647
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-29 14:29:04 -0700

    working on portfolio page

--- Main/src/components/ProjectCard.jsx
+++ Main/src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function ProjectCard({id, title, description, webLink, gitLink, img}) {
+function ProjectCard({id, title, description, webLink, gitLink, img, techArray}) {
     return (
       <Card style={{ width: '18rem', border: '1px solid black', margin: '1rem'}}>
         <Card.Img variant="top" src={img} style={{width: '100%'}} />
@@ -10,6 +10,16 @@ function ProjectCard({id, title, description, webLink, gitLink, img}) {
           <Card.Text>
             {description}
           </Card.Text>
+         {techArray && techArray.length > 0 && (
+          <Card.Text>
+            <strong>Technologies:</strong>
+            <ul>
+              {techArray.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+          </Card.Text>
+        )}
           <a href={webLink} target="_blank">
             <Button variant="primary">Visit Website</Button>
           </a>
--- Main/src/pages/Portfolio.jsx
+++ Main/src/pages/Portfolio.jsx
@@ -1,6 +1,8 @@
 // imports
 import ProjectCard from "../components/ProjectCard";
 import testPic from '../components/test.png';
+import hyperdriveImg from '../assets/projectHyperdrive.png';
+import marsImg from '../assets/projectMars.png';
 
 
 // function
@@ -8,21 +10,21 @@ function Portfolio() {
     const projects = [
     {
         id: 1,
-        title: "project 1",
-        image: testPic,
-        description: 'test for description',
-        webLink: 'https://www.google.co.uk/',
-        gitLink: 'https://github.com/coltondane',
-        techArray: ['']
+        title: "Project Hyper-drive",
+        image: hyperdriveImg,
+        description: "Working alongside a team of four other developers, the motivation for this project came from a desire to produce a product that would be unique, creative, and polished. Our wish was to create an app that challenged us to flex (and build!) our skills that we have learned since beginning our coding journey. Combining a number of technologies as well as concepts that he had learned up to this point, our application functions interactive educational tool aimed at helping people learn about real exoplanets in our galaxy. Utilizing a fun 'game-like' feel  to let users travel virtually through hyperspace.",
+        webLink: 'https://project-hyperdrive-5311751cf71e.herokuapp.com/login',
+        gitLink: 'https://github.com/kalaniJ94/Project_Hyperdrive',
+        techArray: ['JavaScript', 'Tailwind', 'CSS', 'Bootstrap']
     },
     {
         id: 2,
-        title: "project 2",
-        image: testPic,
-        description: '',
-        webLink: '',
-        gitLink: '',
-        techArray: ['']
+        title: "Project Mars",
+        image: marsImg,
+        description: 'This application functions similarly to Project Hyper-Drive, but with a different goal. This was the first project I worked on with other developers, and functioned as a way to work on our HTML, CSS, and JavaScript skills as well as gathering info from an API. This app takes basic facts about our Earth, and compares them to other celestial bodies in the solar system to give users a better understanding of the vastness of space.',
+        webLink: 'https://kalanij94.github.io/Project-Mars/',
+        gitLink: 'https://github.com/kalaniJ94/Project-Mars',
+        techArray: ['JavaScript', 'HTML', 'CSS', 'SCSS']
     },
     {
         id: 3,
@@ -31,7 +33,7 @@ function Portfolio() {
         description: '',
         webLink: '',
         gitLink: '',
-        techArray: ['']
+        techArray: ['JavaScript', 'HTML', 'CSS', 'SCSS']
     },{
         id: 4,
         title: "project 4",
@@ -39,7 +41,7 @@ function Portfolio() {
         description: '',
         webLink: '',
         gitLink: '',
-        techArray: ['']
+        techArray: ['JavaScript', 'HTML', 'CSS', 'SCSS']
     },{
         id: 5,
         title: "project 5",
@@ -47,7 +49,7 @@ function Portfolio() {
         description: '',
         webLink: '',
         gitLink: '',
-        techArray: ['']
+        techArray: ['JavaScript', 'HTML', 'CSS', 'SCSS']
     },{
         id: 6,
         title: "project 6",
@@ -55,7 +57,7 @@ function Portfolio() {
         description: '',
         webLink: '',
         gitLink: '',
-        techArray: ['']
+        techArray: ['JavaScript', 'HTML', 'CSS', 'SCSS']
     }
 ]
     return (
@@ -63,7 +65,7 @@ function Portfolio() {
             <h1>Portfolio</h1>
             <div style={{display: "flex", maxWidth: '1200px', margin: '0 auto', flexWrap: 'wrap'}}>
                 {projects.map(project => (
-                    <ProjectCard key={project.id} title={project.title} description={project.description} webLink={project.webLink} gitLink={project.gitLink} img={project.image}/>
+                    <ProjectCard key={project.id} title={project.title} description={project.description} webLink={project.webLink} gitLink={project.gitLink} img={project.image} techArray={project.techArray}/>
                 ))}
             </div>
         </div>
