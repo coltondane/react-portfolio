e8117793cce36398533789ae601ba2f0d19cbdaa
coltondane <113216904+coltondane@users.noreply.github.com>
2023-11-28 17:24:04 -0700

    working on error not allowing program to work if i use .env variables

--- Main/src/utils/EmailHandler.js
+++ Main/src/utils/EmailHandler.js
@@ -1,17 +1,21 @@
 // component handling emailJS logic on form submit from contact page
 import emailjs from '@emailjs/browser';
 
+const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+
 const sendEmail = async (name, email, message) => {
     try {
         const res = await emailjs.send(
-            "service_4bodsd8",
-            "template_p2nz6nb",
+            serviceId,
+            templateId,
             {
                 from_name: name,
                 from_email: email,
                 message: message,
             },
-            "3k2mEyjS8Of0O5omb"
+            publicKey
         );
 
         console.log("email sent successfully", res);
