afe43a2288901ef91bf643730c460ae4aa79eea4
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-01 16:42:16 -0700

    styling modal

--- Main/src/pages/Contact.jsx
+++ Main/src/pages/Contact.jsx
@@ -17,6 +17,7 @@ function Contact() {
   // state variables for pop up modal
   const [show, setShow] = useState(false);
   const [modalMessage, setModalMessage] = useState('');
+  const [messageType, setMessageType] = useState('');
 
   const handleInputChange = (event) => {
     // Getting the value and name of the input which triggered the change when the form is not submitted
@@ -36,8 +37,8 @@ function Contact() {
   };
 
   // show modal handler
- function showModal(message) {
-  console.log("showing modal");
+ function showModal(message, type) {
+  setMessageType(type);
   setModalMessage(message);
   setShow(true);
 }
@@ -52,7 +53,7 @@ function Contact() {
     event.preventDefault();
     // check if email is valid
     if (!validateEmail(email)) {
-      showModal('Please enter a valid email!');
+      showModal('Please enter a valid email!', 'Email Error');
       return;
     }
     // check if form is filled out
@@ -67,7 +68,7 @@ function Contact() {
         setMessage('');
 
         setFormSubmitted(true);
-        showModal('Thank you for reaching out! I will get back to you as soon as possible :)')
+        showModal('Thank you for reaching out! I will get back to you as soon as possible :)', 'Success')
 
         // reset form submitted state after 5 seconds so they can type again if need be
         setTimeout(() => {
@@ -77,10 +78,10 @@ function Contact() {
 
       } catch (error) {
         console.error(error);
-        showModal('Form failed to reach my email as something went wrong, please try again later!');
+        showModal('Form failed to reach my email as something went wrong, please try again later!', 'Error');
       }
     } else {
-      showModal('Please fill out all fields before submitting!');
+      showModal('Please fill out all fields before submitting!', 'Field Error');
     }
   }
 
@@ -104,11 +105,27 @@ function Contact() {
       </Button>
     </Form>
 
-    <Modal show={show} onHide={closeModal}>
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
+    <Modal 
+      className="modal"
+      show={show} 
+      onHide={closeModal} 
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      style={{ zIndex: 1050 }} >
+      <Modal.Header>
+        <Modal.Title id="contained-modal-title-vcenter" className="modal-title">
+          {messageType}
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>{modalMessage}</Modal.Body>
+      <Modal.Body>
+        <p>
+          {modalMessage}
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={closeModal}>Close</Button>
+      </Modal.Footer>
     </Modal>
     </>
   );
