87946f1882ae877970dacedddb608fec04414634
coltondane <113216904+coltondane@users.noreply.github.com>
2023-12-01 15:14:32 -0700

    removing the alert element and replacing them with bootstrap modals

--- Main/src/pages/Contact.jsx
+++ Main/src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
 import sendEmail from "../utils/EmailHandler";
 
