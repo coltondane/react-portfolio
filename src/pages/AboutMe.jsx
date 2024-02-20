eec4f58a6095aa58031328909589cd95ffc7625d
coltondane <113216904+coltondane@users.noreply.github.com>
2024-02-19 18:42:17 -0700

    continuation

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -10,12 +10,15 @@ function AboutMe() {
             <div className="content">
                 <p>
                     As a passionate and dedicated Full Stack Software Engineer with a background in Biochemistry and Art, I bring a unique blend of skills and experiences to every project I undertake.
-
+                </p>
+                <p>
                     My portfolio showcases a diverse range of projects, each highlighting my proficiency in modern software technologies and languages such as JavaScript, Python, React, SQL, etc. From my first HTML/CSS/JAVA project to the most recent involving authentication, React, and more. I have consistently demonstrated my ability to write clean, concise code.
-
+                </p>
+                <p>
                     I am committed to delivering high-quality work that exceeds expectations and drives tangible results. With a strong focus on Innovation, Adaptability, and Continuous Learning, I strive to make a meaningful impact in every endeavor.
-
-                    Browse through my portfolio, or click the links below to explore my past projects and discover how I can contribute to your team both as a developer and as a creative thinker.
+                </p>
+                <p>
+                    Browse through my portfolio, or click the links below to explore my past projects and discover how I can contribute to your team both as a developer and as a creative thinker. And feel free to reach out to me with any questions or opportunities directly through the 'Contact Me' page.
                 </p>
             </div>
         </div>
